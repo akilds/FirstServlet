8b3bb2154c8089771b69bf89338944fadacd99f6
akilds <akilrainadesigan@gmail.com>
2021-05-14 21:08:09 +0530

    ADDED: Creation of First Servlet

--- src/main/java/com/myservlet/SimpleServlet.java
+++ src/main/java/com/myservlet/SimpleServlet.java
@@ -1,7 +1,21 @@
 package com.myservlet;
 
-public class SimpleServlet {
-    public static void main(String[] args) {
-        System.out.println("My First Servlet");
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World New Try!</h3>");
+        out.close();
     }
 }
